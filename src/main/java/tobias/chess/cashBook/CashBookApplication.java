bb5ec123f9f6a7dd604064a4bbc87a95743d8d1c
Tobias Göttel <tobias.goettel@daimler.com>
2020-05-30 09:50:24 +0200

    Initial commit

---
+++ src/main/java/tobias/chess/cashBook/CashBookApplication.java
@@ -0,0 +1,13 @@
+package tobias.chess.cashBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tobias/chess/cashBook/CashBookApplicationTests.java
@@ -0,0 +1,13 @@
+package tobias.chess.cashBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
